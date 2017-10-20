b742044b92874564496089ffa52c323b275fd687
David Seywald <d.seywald@student.tugraz.at>
2017-10-20 14:17:14 +0200

    L1 start

---
+++ L1/C#/Account.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace C_
+{
+    public class Account
+    {
+        public long Balance {get;set;}
+        public string User {get;set;}
+    }
+}
--- L1/C#/Program.cs
+++ L1/C#/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace C_
 {
@@ -7,6 +8,19 @@ namespace C_
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int numberOfAccounts = int.Parse(Console.ReadLine());
+
+            List<Account> accounts = new List<Account>(numberOfAccounts);
+            for(int i = 0; i < numberOfAccounts; i++)
+            {
+                string[] tmp = Console.ReadLine().Split(' ');
+                string person = tmp[0].Trim();
+                long balance = long.Parse(tmp[1].Trim());
+                Account a = new Account{User = person, Balance = balance};
+                accounts.Add(a);
+            }
+            int numberOfTransactions = int.Parse(Console.ReadLine());
         }
     }
 }
