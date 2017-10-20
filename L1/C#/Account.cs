7adc3138ea379e7e0d6b6a65cd06493d2598e864
David Seywald <d.seywald@student.tugraz.at>
2017-10-20 15:20:59 +0200

    Validate

--- L1/C#/Account.cs
+++ L1/C#/Account.cs
@@ -6,5 +6,7 @@ namespace C_
     {
         public long Balance {get;set;}
         public string User {get;set;}
+        public string AccountNumber {get;set;}
+        public long Limit {get;set;}
     }
 }
--- L1/C#/Program.cs
+++ L1/C#/Program.cs
@@ -19,10 +19,16 @@ namespace C_
             {
                 string[] tmp = Console.ReadLine().Split(' ');
                 string person = tmp[0].Trim();
-                long balance = long.Parse(tmp[1].Trim());
-                Account a = new Account{User = person, Balance = balance};
+                string accountNumber = tmp[1].Trim();
+                long balance = long.Parse(tmp[2].Trim());
+                long limit = long.Parse(tmp[3].Trim());
+                Account a = new Account{User = person, Balance = balance, AccountNumber = accountNumber, Limit = limit};
                 accounts.Add(a);
             }
+
+            //Filter invalid account
+            accounts = accounts.Where(x => AccountValidator.Validate(x.AccountNumber)).ToList();
+
             int numberOfTransactions = int.Parse(Console.ReadLine());
             List<Transaction> transactions = new List<Transaction>(numberOfTransactions);
             for(int i = 0; i < numberOfTransactions; i++)
@@ -39,11 +45,18 @@ namespace C_
             var orderedTransactions = transactions.OrderBy(x => x.Time);
             foreach(var t in orderedTransactions)
             {
-                var fromAccount = accounts.Where(x => x.User == t.From).First();
-                var toAccount = accounts.Where(x => x.User == t.To).First();
+                var fromAccount = accounts.Where(x => x.AccountNumber == t.From).FirstOrDefault();
+                var toAccount = accounts.Where(x => x.AccountNumber == t.To).FirstOrDefault();
 
-                fromAccount.Balance -= t.Amount;
-                toAccount.Balance += t.Amount;
+                //Check if accounts are valid
+                if(fromAccount != null && toAccount != null){
+                    long maxMoney = fromAccount.Balance + fromAccount.Limit;
+                    if(maxMoney <= t.Amount)
+                    {
+                        fromAccount.Balance -= t.Amount;
+                        toAccount.Balance += t.Amount;
+                    }
+                }
             }
 
             StringBuilder sb = new StringBuilder();
@@ -53,8 +66,41 @@ namespace C_
                 sb.Append(a.User).Append(" ").Append(a.Balance).Append(Environment.NewLine);
             }
 
-            File.WriteAllText("level1-4.txt", sb.ToString());
+            File.WriteAllText("level2-1.txt", sb.ToString());
 
+        }
+
+        public static bool Validate(string accountNumber)
+        {
+            if(string.IsNullOrWhiteSpace(accountNumber))
+            {
+                return false;
+            }
+            if(accountNumber.Length != 15)
+                return false;
+            if(!accountNumber.StartsWith("CAT"))
+                return false;
+            
+            char check1 = accountNumber[3];
+            char check2 = accountNumber[4];
+
+            if(!char.IsDigit(check1) || !char.IsDigit(check2))
+                return false;
+
+            System.Console.WriteLine("Checking :" + accountNumber);
+            var distinctChars = accountNumber.Skip(5).Distinct();
+            foreach(char c in distinctChars)
+            {
+                System.Console.WriteLine("Checking Letter: " + c);
+                int count1 = accountNumber.Count(x => x == c);
+                System.Console.WriteLine("Found: " + count1);
+                int count2 = accountNumber.Count(x => x == char.ToUpper(c));
+                System.Console.WriteLine("Found Upper: " + count2);
+                if(count1 != count2)
+                    return false;
+            }
+
+            return true;
         }
     }
 }
